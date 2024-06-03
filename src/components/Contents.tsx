b072cf04e3f7c7077eafbeed89be4faf20869b34
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-06-03 19:23:17 +0900

    create view

---
+++ src/components/Contents.tsx
@@ -0,0 +1,14 @@
+import CheckBox from '@/components/CheckBox'
+import Graph from '@/components/Graph'
+import '@/styles/contents.css'
+
+function Contents() {
+  return (
+    <div id="contents-items">
+      <CheckBox />
+      <Graph />
+    </div>
+  )
+}
+
+export default Contents
---
+++ src/components/Graph.tsx
@@ -0,0 +1,11 @@
+import '@/styles/graph.css'
+
+function Graph() {
+  return (
+    <div id="graph-area">
+      <div>Graph</div>
+    </div>
+  )
+}
+
+export default Graph
