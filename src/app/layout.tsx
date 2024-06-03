f0f9ec4e91faef2a0c06eeaa8b64af44a34f6640
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-06-03 17:53:33 +0900

    setting linter & pretter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,21 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "YUMEMI-Coding-Test",
-  description: "株式会社YUMEMIの2weeksインターンのコーディングテスト",
-};
+  title: 'YUMEMI-Coding-Test',
+  description: '株式会社YUMEMIの2weeksインターンのコーディングテスト',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="ja">
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
