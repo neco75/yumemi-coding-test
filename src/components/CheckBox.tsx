9d3d7c430cf46e556e850a318c2730c686c5be7e
sagara_rikuto <sagara_rikuto@plise.co.jp>
2024-06-03 21:27:06 +0900

    都道府県一覧のチェックボックスを動的に生成する

--- src/components/CheckBox.tsx
+++ src/components/CheckBox.tsx
@@ -1,15 +1,47 @@
+'use client'
 import '@/styles/checkBox.css'
-import checkBoxes from '@/utils/checkBoxesType'
+import { useEffect, useState } from 'react'
 
 function CheckBox() {
+  const [prefectures, setPrefectures] = useState([])
+
+  async function fetchPrefectures() {
+    try {
+      const headers = new Headers()
+      headers.set('X-API-KEY', process.env.NEXT_PUBLIC_API_KEY as string)
+      const response = await fetch(
+        'https://opendata.resas-portal.go.jp/api/v1/prefectures',
+        {
+          headers: headers,
+        },
+      )
+
+      const data = await response.json()
+      console.log(data)
+      setPrefectures(
+        data.result.map((item: { prefCode: number; prefName: string }) => ({
+          prefCode: item.prefCode,
+          prefName: item.prefName,
+        })),
+      )
+    } catch (error) {
+      console.error('Error fetching prefectures:', error)
+      return []
+    }
+  }
+
+  useEffect(() => {
+    fetchPrefectures()
+  }, [])
+
   return (
     <div id="input-area">
       <h1>都道府県</h1>
       <div id="check-boxes">
-        {checkBoxes.map((letter) => (
-          <div className="check-box" key={letter.id}>
+        {prefectures.map((prefecture: prefecture) => (
+          <div className="check-box" key={prefecture.prefCode}>
             <input type="checkbox" />
-            <div>{letter.name}</div>
+            <div>{prefecture.prefName}</div>
           </div>
         ))}
       </div>
